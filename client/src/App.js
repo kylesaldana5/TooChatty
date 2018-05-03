2a7d6efccadcf03734ca5b84e6086fb0e2c82f47
kylesaldana5 <kylesaldana5@gmail.com>
2018-05-03 11:00:57 -0500

    first commit / file structure set up

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div>
+        <p>
+          Hello World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
